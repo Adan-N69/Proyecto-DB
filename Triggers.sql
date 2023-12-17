df8cc4b8af4394a0e50d054f97a4189d6dc6c084
RiskYcH <152217498+RiskYcH@users.noreply.github.com>
2023-12-16 19:22:35 -0600

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -22,23 +22,3 @@ END;
 
 
 
-EXEC Agregar_Estudiante
-'2023654707',
-'Jose Luis',
-'Carrillo',
-'Zuarez',
-1,
-1;
-
-
-select* from Estudiante
-select* from [dbo].[Kardex]
-select * from [dbo].[Horario_Sugerido]
-delete from [dbo].[Horario_Sugerido]
-delete from [dbo].[Kardex]
-delete from [dbo].[Estudiante]
-
-
-select * from Horario_Sugerido HS 
-INNER JOIN [dbo].[Curso_Horario] CH ON HS.ID_Curso = CH.ID_Curso
-INNER JOIN [dbo].[Horario] H ON H.Clave_Horario = CH.Clave_Horario
