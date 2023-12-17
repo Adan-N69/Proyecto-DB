689613580a3503f05b6fe06162954dd7d870e721
RiskYcH <152217498+RiskYcH@users.noreply.github.com>
2023-12-16 20:52:53 -0600

    Update and rename horarioPersonal.sql to horarioMixto.sql

--- horarioMixto.sql
+++ horarioMixto.sql
@@ -4,6 +4,7 @@ CREATE OR ALTER PROCEDURE Obtener_Cursos
 	BEGIN
 		---tabla temporal para guardar los cursos
 		CREATE TABLE #cursos(
+		ID int,
 		Grupo varchar(5),
 		Materia varchar(80),
 		Profesor varchar(70),
@@ -24,6 +25,7 @@ CREATE OR ALTER PROCEDURE Obtener_Cursos
 		WHILE @@FETCH_STATUS = 0
 		BEGIN
 			SET @ClaveMateria =( SELECT Clave FROM Materia WHERE Nombre_Materia = @Materia);
+
 			---Se verifica que esa materia no este registrada en kardex
 			IF NOT EXISTS(SELECT 1 FROM [dbo].[Materias_Cursadas] WHERE Clave_Materia = @ClaveMateria and ID_Kardex = @Kardex)
 			BEGIN 
@@ -38,8 +40,8 @@ CREATE OR ALTER PROCEDURE Obtener_Cursos
 
 					IF(@Cont_Elegibles < 2)
 					BEGIN
-						INSERT INTO #cursos(Grupo, Materia,Profesor, Dia , Hora)
-						SELECT  C.Grupo, M.Nombre_Materia, P.P_Apellido_P+' '+P.P_Apellido_M+' '+P.P_Nombre AS Profesor, H.Dia, H.Hora FROM Curso C
+						INSERT INTO #cursos(ID,Grupo, Materia,Profesor, Dia , Hora)
+						SELECT C.[ID_Curso], C.Grupo, M.Nombre_Materia, P.P_Apellido_P+' '+P.P_Apellido_M+' '+P.P_Nombre AS Profesor, H.Dia, H.Hora FROM Curso C
 						INNER JOIN Curso_Horario CH ON CH.ID_Curso = C.ID_Curso
 						INNER JOIN Horario H ON H.Clave_Horario = CH.Clave_Horario
 						INNER JOIN Materia M ON M.Clave = C.Clave_Materia
@@ -58,8 +60,8 @@ CREATE OR ALTER PROCEDURE Obtener_Cursos
 
 					IF(@Cont_Optativas1 < 1)
 					BEGIN
-						INSERT INTO #cursos(Grupo, Materia,Profesor, Dia , Hora)
-						SELECT  C.Grupo, M.Nombre_Materia, P.P_Apellido_P+' '+P.P_Apellido_M+' '+P.P_Nombre AS Profesor, H.Dia, H.Hora FROM Curso C
+						INSERT INTO #cursos(ID,Grupo, Materia,Profesor, Dia , Hora)
+						SELECT C.[ID_Curso], C.Grupo, M.Nombre_Materia, P.P_Apellido_P+' '+P.P_Apellido_M+' '+P.P_Nombre AS Profesor, H.Dia, H.Hora FROM Curso C
 						INNER JOIN Curso_Horario CH ON CH.ID_Curso = C.ID_Curso
 						INNER JOIN Horario H ON H.Clave_Horario = CH.Clave_Horario
 						INNER JOIN Materia M ON M.Clave = C.Clave_Materia
@@ -77,8 +79,8 @@ CREATE OR ALTER PROCEDURE Obtener_Cursos
 
 					IF(@Cont_Optativas2 < 1)
 					BEGIN
-						INSERT INTO #cursos(Grupo, Materia,Profesor, Dia , Hora)
-						SELECT  C.Grupo, M.Nombre_Materia, P.P_Apellido_P+' '+P.P_Apellido_M+' '+P.P_Nombre AS Profesor, H.Dia, H.Hora FROM Curso C
+						INSERT INTO #cursos(ID,Grupo, Materia,Profesor, Dia , Hora)
+						SELECT C.[ID_Curso], C.Grupo, M.Nombre_Materia, P.P_Apellido_P+' '+P.P_Apellido_M+' '+P.P_Nombre AS Profesor, H.Dia, H.Hora FROM Curso C
 						INNER JOIN Curso_Horario CH ON CH.ID_Curso = C.ID_Curso
 						INNER JOIN Horario H ON H.Clave_Horario = CH.Clave_Horario
 						INNER JOIN Materia M ON M.Clave = C.Clave_Materia
@@ -90,8 +92,8 @@ CREATE OR ALTER PROCEDURE Obtener_Cursos
 				--si no es ninguna
 				ELSE
 				BEGIN
-					INSERT INTO #cursos(Grupo, Materia,Profesor, Dia , Hora)
-						SELECT  C.Grupo, M.Nombre_Materia, P.P_Apellido_P+' '+P.P_Apellido_M+' '+P.P_Nombre AS Profesor, H.Dia, H.Hora FROM Curso C
+						INSERT INTO #cursos(ID,Grupo, Materia,Profesor, Dia , Hora)
+						SELECT C.[ID_Curso], C.Grupo, M.Nombre_Materia, P.P_Apellido_P+' '+P.P_Apellido_M+' '+P.P_Nombre AS Profesor, H.Dia, H.Hora FROM Curso C
 						INNER JOIN Curso_Horario CH ON CH.ID_Curso = C.ID_Curso
 						INNER JOIN Horario H ON H.Clave_Horario = CH.Clave_Horario
 						INNER JOIN Materia M ON M.Clave = C.Clave_Materia
@@ -111,21 +113,22 @@ CREATE OR ALTER PROCEDURE Obtener_Cursos
 		SELECT * FROM #cursos
 END;
 
-EXEC Obtener_Cursos 6;
 
 
-CREATE OR ALTER PROCEDURE Agregar_CursoHSP
-	@Grupo varchar(5),
-	@Materia varchar(80),
-	@Id_Kardex  int
+
+CREATE OR ALTER PROCEDURE Validar_SecuenciaMixto
+	@ID_Curso int,
+	@Id_Kardex  int,
+	@Validacion bit OUTPUT
 
 	AS
 	BEGIN
 
 		DECLARE @Total_Secuencia int;
 		DECLARE @Cont_Cursadas int;
-		DECLARE @Clave_Materia varchar(4);
+		DECLARE @Clave_Materia varchar(4) = (SELECT Clave_Materia FROM Curso WHERE ID_Curso = @ID_Curso);
 
+		
 		SELECT @Total_Secuencia = COUNT(*)
 		FROM Secuencias
 		WHERE Materia_Base = @Clave_Materia;
@@ -135,15 +138,56 @@ CREATE OR ALTER PROCEDURE Agregar_CursoHSP
 		INNER JOIN [dbo].[Secuencias]  S ON  M.[Clave_Materia] = S.Materia_Necesaria
 		WHERE Materia_Base = @Clave_Materia;
 
-		IF(@Cont_Cursadas = @Total_Secuencia)
+		IF(@Cont_Cursadas = @Total_Secuencia) --- valida si tiene todas las secuencias
 		BEGIN
-			print('insertar en horario sugerido')
+			SET @Validacion = 0;
 		END
 
-		ELSE
+		ELSE --- No tiene todas las validaciones
 		BEGIN 
-			print('preguntar al usuario')
+			 SET @Validacion = 1;
+			--llamar en java mnesje
+		--si respuesta es Si insertar
+		---
 		END
-		
+	
 END;
+
+
+CREATE OR ALTER PROCEDURE Agregar_CursoHSP
+	@ID_Curso int,
+	@Id_Kardex  int,
+	@Validacion bit OUTPUT
+	AS
+		BEGIN
+---validar traslapes
+--validar si la tabla horario sugerido ya tiee cursos
+			DECLARE @CursosActuales int;
+			SET @CursosActuales = (SELECT COUNT(*) FROM Horario_Sugerido WHERE Kardex_HS = @Id_Kardex);
+		
+			if(@CursosActuales = 0) ---si estaba vacia 
+				begin
+					INSERT INTO Horario_Sugerido(Kardex_HS, ID_Curso, ID_Tipo_Horario)
+					VALUES(@Id_Kardex,@ID_Curso,3);
+	
+				end
+
+				else  --- no esta la tabla vacia
+				begin
+					IF NOT EXISTS(SELECT Clave_Horario FROM [dbo].[Curso_Horario] WHERE ID_Curso = @ID_Curso INTERSECT
+						SELECT CH.Clave_Horario FROM Horario_Sugerido HS INNER JOIN Curso_Horario CH 
+						ON HS.ID_Curso = CH.ID_Curso WHERE HS.Kardex_HS = @Id_Kardex)	
+					BEGIN	
+						INSERT INTO Horario_Sugerido(Kardex_HS, ID_Curso, ID_Tipo_Horario)VALUES(@Id_Kardex,@ID_Curso,3);
+						SET @Validacion = 0;
+					END -- IF NOT
+
+					ELSE --- hay translapes
+					BEGIN
+						SET @Validacion = 1;
+						---mostrar msj en java
+					END
+
+				END
+		END;
 
